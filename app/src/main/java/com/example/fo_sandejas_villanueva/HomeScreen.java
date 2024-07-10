3d0c3a7e79ea1b9dffe37ce34535b6e0ecf2aea8
sandsocks <max.sandejas@gmail.com>
2024-07-11 00:06:45 +0800

    added some stuff to the ui

--- app/src/main/java/com/example/fo_sandejas_villanueva/HomeScreen.java
+++ app/src/main/java/com/example/fo_sandejas_villanueva/HomeScreen.java
@@ -1,4 +1,12 @@
 package com.example.fo_sandejas_villanueva;
 
-public class HomeScreen {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity {
 }
