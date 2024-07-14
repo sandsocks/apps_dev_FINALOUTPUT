bed4a3d588a72c23b1d783440207428346108324
sandsocks <max.sandejas@gmail.com>
2024-07-14 16:24:15 +0800

    moved welcome page content to main activity

--- app/src/main/java/com/example/fo_sandejas_villanueva/MainActivity.java
+++ app/src/main/java/com/example/fo_sandejas_villanueva/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.fo_sandejas_villanueva;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.welcome_screen);
+
+        Button button=findViewById(R.id.login_registerButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent(MainActivity.this,LoginRegisterScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
